bbf5880fe578272f4b45725c625a1d175a68f10c
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-05-24 15:51:30 -0400

    solved counter

---
+++ javascript/counter.js
@@ -0,0 +1,11 @@
+//  Given an integer n, return a counter function.
+// This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+var createCounter = function (n) {
+  counter = 0;
+  return function () {
+    num = n + counter;
+    counter++;
+    return num;
+  };
+};
