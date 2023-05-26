8c93728a9aa57fb29801c48ba48b4a563850a9af
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-05-26 10:56:53 -0400

    solved gcdOfStrings

--- javascript/gcdOfStrings.js
+++ javascript/gcdOfStrings.js
@@ -7,19 +7,45 @@ both str1 and str2.
 */
 
 var gcdOfStrings = function (str1, str2) {
-  let gcd = "";
-  const str1Length = str1.length;
-  const str2Length = str2.length;
-  const length = Math.max(str1Length, str2Length);
-  for (let i = 0; i < length; i++) {
-    // if gcd.length multiplied many times equals str1 or str2 return gcd
-    if (str1[i] === str2[i]) {
-      gcd = gcd + str1[i];
-    } else {
+  // check if there is a GCD of lengths
+  let a = str1.length;
+  let b = str2.length;
+  while (b !== 0) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  let numGCD = a;
+  console.log(numGCD);
+
+  //   check if substrings of each string match
+  if (str1.substring(0, numGCD) === str2.substring(0, numGCD)) {
+    let gcd = str1.substring(0, numGCD);
+    // get # of times to repeat the substring
+    let numberOfTimes1 = str1.length / numGCD;
+    let numberOfTimes2 = str2.length / numGCD;
+    // check to see if the repeated substrings match the originals
+    if (
+      gcd.repeat(numberOfTimes1) === str1 &&
+      gcd.repeat(numberOfTimes2) === str2
+    ) {
       return gcd;
+    } else {
+      return "";
     }
+  } else {
+    return "";
   }
 };
 
-const test = gcdOfStrings("ABABAB", "ABAB");
+// 360
+let string1 =
+  "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+
+let string2 =
+  "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+
+let string3 = "ABABABAB";
+let string4 = "ABABAC";
+const test = gcdOfStrings(string3, string4);
 console.log(test);
