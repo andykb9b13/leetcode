fe2333b7d8cadb135e6997f034bd743297decca6
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-06-04 11:55:56 -0400

    working on isSubsequence

--- javascript/isSubsequence.js
+++ javascript/isSubsequence.js
@@ -9,3 +9,25 @@ by deleting some (can be none) of the characters without disturbing
 the relative positions of the remaining characters. 
 (i.e., "ace" is a subsequence of "abcde" while "aec" is not). 
 */
+
+var isSubsequence = function (s, t) {
+  let temp = 0;
+  for (let i = 0; i < s.length; i++) {
+    console.log(`indexOf ${i}`, string2.indexOf(string1[i]));
+    if (t.indexOf(s[i], temp) >= temp) {
+      console.log("temp at beginning", temp);
+      console.log(string2.indexOf(string1[i]));
+      temp = t.indexOf(s[i]);
+      console.log("temp at end", temp);
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
+
+const string1 = "aaaaaa";
+const string2 = "bbaaaa";
+
+const test = isSubsequence(string1, string2);
+console.log("Test", test);
